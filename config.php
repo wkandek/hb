f74806ddcdfbec09b92eebdf0ae6d825aa68c3ec
root <root@UbuWS.(none)>
2015-01-02 02:06:30 +0000

    adding original files

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ $mysql_user = "root";
 $mysql_password = "heartbleed";
 $mysql_database = "hb";
 
-$bd = new mysqli($mysql_hostname, $mysql_user, $mysql_password, $mysql_database ) or die("Opps some thing went wrong");
+$bd = new mysqli($mysql_hostname, $mysql_user, $mysql_password, $mysql_database ) or die("Oops some thing went wrong");
 
 ?>
